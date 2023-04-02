0971c9473f92351f7b14d20dcc763ad4d6cf09dd
xtej4ik <chtejinna@gmail.com>
2023-04-02 22:38:13 +0100

    Ticket 12

--- models/topics.models.js
+++ models/topics.models.js
@@ -9,7 +9,13 @@ exports.fetchTopics = () =>{
       });
 }
 exports.fetchArticleById = (article_id) => {
-    return db.query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
+    return db.query(`
+    SELECT articles.*, 
+    COUNT(comments.comment_id) ::INT AS comment_count
+    FROM articles
+    LEFT JOIN comments ON articles.article_id = comments.article_id
+    WHERE articles.article_id = $1
+    GROUP BY articles.article_id`, [article_id])
       .then(result => {
         if (result.rows.length === 0) {
           return Promise.reject({ status: 404, msg: `Article ${article_id} not found` });
